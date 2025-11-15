99ea64a6bf88bc8d62c4ed37ed1baf2a567f3f9f
Angel97Figueroa <angel.figueroa97@uabc.edu.mx>
2025-11-14 16:25:45 -0800

    Ya es este es el ultimo testeo

--- hola.c
+++ hola.c
@@ -7,5 +7,6 @@ int main(){
     printf("ok, esta si es la de verdad XD\n");
     printf("Ahora con el git integrado de vs code\n");
     printf("No preguntes por que tanto testeo\n");
+    printf("Ya este es el ultimo testeo xd\n");
     return 0;
 }
