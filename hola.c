99a24458e5ed3a9c6cc5edbef97319ee22bc2cb8
Angel97Figueroa <angel.figueroa97@uabc.edu.mx>
2025-11-14 16:24:18 -0800

    Ok, ya casi termino de probarlo

--- hola.c
+++ hola.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 int main(){
     printf("Hola mundo\n");
     printf("Hola Diego\n");
     printf("Ahora si, esta es la buena\n");
     printf("ok, esta si es la de verdad XD\n");
     printf("Ahora con el git integrado de vs code\n");
+    printf("No preguntes por que tanto testeo\n");
     return 0;
 }
